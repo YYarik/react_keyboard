32d8afb3a71fc21d6f3e7af091f889ff39401fc4
Yaroslav <gaubetsa@gmail.com>
2026-05-16 23:26:23 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export class App extends React.Component<{}, AppState> {
 
     return (
       <div className="App">
-        <p className="App_message">{message}</p>
+        <p className="App__message">{message}</p>
       </div>
     );
   }
